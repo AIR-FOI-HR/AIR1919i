af9e87444f67960780df295c4f42f53b4b82333f
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:47:44 +0100

    Backend/app/Rating: [add] - users, meal relationships

--- Backend/app/Rating.php
+++ Backend/app/Rating.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
+    /**
+     * Rating HAS ONE User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function users()
+    {
+        return $this->hasOne(User::class);
+    }
 
+    /**
+     * Rating HAS ONE Meal
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function meal()
+    {
+        return $this->hasOne(Meal::class);
+    }
 }
