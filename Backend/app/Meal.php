3ab046f28a8313749101eff596dfd5a8286e588f
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:45:50 +0100

    Backend/app/Meal: [add] - protected fillable name, users & ratings relationships

--- Backend/app/Meal.php
+++ Backend/app/Meal.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
 
+    /**
+     * Meal HAS MANY users favorites
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_favorite_meals');
+    }
+
+    /**
+     * Meal HAS MANY ratings
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function ratings()
+    {
+        return $this->belongsToMany(Rating::class, 'ratings');
+    }
 }
