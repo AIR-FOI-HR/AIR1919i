ba234723db4e01f170a504b49b7aa33eaa901d32
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:46:23 +0100

    Backend/app/QRCode: [add] - users relationship

--- Backend/app/QRCode.php
+++ Backend/app/QRCode.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class QRCode extends Model
 {
-
+    /**
+     * QRCode HAS MANY Users
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'qr_code_user');
+    }
 }
