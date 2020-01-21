519ae979e9dc26ae49e53696b90e4c31a8484c54
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:28:53 +0100

    Backend/database/migrations: [create] - qr_codes table

---
+++ Backend/database/migrations/2020_01_21_200453_qr_codes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class QrCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qr_codes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('code');
+            $table->tinyInteger('times_scanned')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qr_codes');
+    }
+}
