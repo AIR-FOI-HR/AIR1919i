9c56de0d5bfe93f408d92f9aced356ad0ffcbbb5
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:39:22 +0100

    Backend/app/Console/Commands: [create] - InsertNewRecords command for faster db dummy data inserting

---
+++ Backend/app/Console/Commands/InsertNewRecords.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Meal;
+use App\User;
+use Illuminate\Console\Command;
+
+class InsertNewRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'records:insert';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Inserts new records in the database.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Meal::create(['name' => 'Pizza']);
+
+        User::find(1)->favoriteMeals()->save(Meal::first());
+
+        User::find(1)->ratings()->save(Meal::first(), ['stars' => 5, 'comment' => 'Very good food indeed.']);
+    }
+}
