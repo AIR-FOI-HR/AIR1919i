4c258f6d7582da8c313aa7765fbca1f108cc7e12
Toni Krešić <tonikresic1997@gmail.com>
2020-01-21 21:44:43 +0100

    Backend/app/User: [add] - favoriteMeals, ratings, QRCodes relationships

--- Backend/app/User.php
+++ Backend/app/User.php
@@ -2,9 +2,8 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -36,4 +35,34 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * User HAS MANY favorite meals
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function favoriteMeals()
+    {
+        return $this->belongsToMany(Meal::class, 'user_favorite_meals');
+    }
+
+    /**
+     * User HAS MANY ratings
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function ratings()
+    {
+        return $this->belongsToMany(Rating::class, 'ratings');
+    }
+
+    /**
+     * User HAS MANY QRCodes
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function QRCodes()
+    {
+        return $this->belongsToMany(QRCode::class, 'qr_code_user');
+    }
 }
