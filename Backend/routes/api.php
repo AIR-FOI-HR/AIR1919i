31813dab13d074862e89584b7f4c4a0ad51760b8
Toni Krešić <tonikresic1997@gmail.com>
2020-01-30 20:15:20 +0100

    web.php, api.php: [create] - api authentication routes and meals resource api

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,18 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+// Auth Endpoints
+Route::group([
+    'prefix' => 'v1/auth',
+], function () {
+    Route::post('login', 'Api\LoginApiController@login');
+    Route::post('logout', 'Api\LogoutApiController@logout');
+    Route::post('register', 'Api\RegisterApiController@register');
+    Route::post('forgot-password', 'Api\ForgotPasswordApiController@email');
+    Route::post('password-reset', 'Api\ResetPasswordApiController@reset');
+});
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Resource Endpoints
+Route::group([
+    'prefix' => 'v1'
+], function () {
+    Route::apiResource('meals', 'Api\MealApiController');
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Not Found
+Route::fallback(function(){
+    return response()->json(['message' => 'Resource not found.'], 404);
 });
