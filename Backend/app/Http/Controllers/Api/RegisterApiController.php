90d1b4cc47ae16379f54b8a3a03e9dbe4f3bcf28
Toni Krešić <tonikresic1997@gmail.com>
2020-01-30 20:23:54 +0100

    Backend/app/Http/Controllers/Api: [create] - RegisterApiController

---
+++ Backend/app/Http/Controllers/Api/RegisterApiController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class RegisterApiController extends APIController
+{
+    use RegistersUsers;
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) return $this->responseUnprocessable($validator->errors());
+
+        try {
+            $this->create($request->all());
+            return $this->responseSuccess('Registered successfully.');
+        } catch (\Exception $e) {
+            return $this->responseServerError('Registration error.');
+        }
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
