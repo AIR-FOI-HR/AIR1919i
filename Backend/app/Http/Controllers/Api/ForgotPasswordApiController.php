7bac8773d9e0519d6b8fca31dadf377c15ba6670
Toni Krešić <tonikresic1997@gmail.com>
2020-01-30 20:22:43 +0100

    Backend/app/Http/Controllers/Api: [create] - ForgotPasswordController

---
+++ Backend/app/Http/Controllers/Api/ForgotPasswordApiController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordApiController extends APIController
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
+
+    use SendsPasswordResetEmails;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function email(Request $request)
+    {
+        $validator = Validator::make(
+            $request->only('email'),
+            ['email' => 'required|string|email|max:255|exists:users,email'],
+            ['exists' => "We couldn't find an account with that email."]
+        );
+
+        if ($validator->fails()) return $this->responseUnprocessable($validator->errors());
+
+        $response = $this->sendResetLinkEmail($request);
+
+        return $response ? $this->responseSuccess('Email reset link sent.') : $this->responseServerError();
+    }
+}
