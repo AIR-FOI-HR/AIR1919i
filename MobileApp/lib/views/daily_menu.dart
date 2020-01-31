1d8e2ddfa853ae9a0ed446a351654302dd88c050
clakupres <clakupres@foi.hr>
2020-01-31 22:34:13 +0100

    Implementacija pregleda dnevnog jelovnika

--- MobileApp/lib/views/daily_menu.dart
+++ MobileApp/lib/views/daily_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/models/meal.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile_app/providers/meal.dart';
 import 'package:mobile_app/widgets/meal_list.dart';
@@ -16,18 +17,57 @@ class DailyMenuState extends State<DailyMenu> {
     String cameraScanResult = await scanner.scan();
     print('Printing QR Code Scan Result');
     print(cameraScanResult);
-    // TODO => Send API request to backend, validate the request and return the result.
+    // TODO => Send (authenticated) API request to backend, validate the request and return the result.
   }
 
   @override
   Widget build(BuildContext context) {
 
-    final meals = Provider.of<MealProvider>(context).meals;
+//    final meals = Provider.of<MealProvider>(context).meals;
+
+    final meal_1 = new Meal();
+    meal_1.id = 1;
+    meal_1.name = 'Hamburger';
+    meal_1.description = 'Very tasty.';
+
+    final meal_2 = new Meal();
+    meal_2.id = 1;
+    meal_2.name = 'Salat';
+    meal_2.description = 'Much expensive.';
+
+    final meal_3 = new Meal();
+    meal_3.id = 1;
+    meal_3.name = 'Pizza';
+    meal_3.description = 'Such delicious.';
+
+    List<Meal> meals = List<Meal>(3);
+
+    meals[0] = meal_1;
+    meals[1] = meal_2;
+    meals[2] = meal_3;
 
     return Scaffold(
       appBar: AppBar(title: Text('Daily Menu'), backgroundColor: Colors.black87),
-      body: Center(
-        child: mealList(context, meals),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Dan", style: new TextStyle(
+                  fontSize: 23.0, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Datum", style: new TextStyle(
+                  fontSize: 18.0, fontWeight: FontWeight.normal)),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10.0,0,10.0,0),
+                child: mealList(context, meals))
+          ],
+        ),
       ),
       drawer: NavigationDrawer(),
       floatingActionButton: FloatingActionButton(
--- MobileApp/lib/widgets/meal_list.dart
+++ MobileApp/lib/widgets/meal_list.dart
@@ -1,19 +1,124 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_app/models/meal.dart';
 
 Widget mealList(BuildContext context, List<Meal> meals) {
-
   return ListView.separated(
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
     itemCount: meals.length,
     itemBuilder: (BuildContext context, int index) {
       final meal = meals[index];
-      return ListTile(
-        key: Key((meal.id).toString()),
-        title: Text(meal.name),
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+        child: new Card(
+          elevation: 10,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 25,
+                color: Colors.red,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 0, 0, 5),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(6.0),
+                              child: Image.asset("assets/images/hamburger.jpg",
+                                  width: 150)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 5, 0, 10),
+                          child: new Row(
+                            //mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.star, color: Colors.yellow[500],size: 16.0),
+                              Icon(Icons.star, color: Colors.yellow[500],size: 16.0),
+                              Icon(Icons.star, color: Colors.yellow[500],size: 16.0),
+                              Icon(Icons.star, color: Colors.grey,size: 16.0),
+                              Icon(Icons.star, color: Colors.grey,size: 16.0),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(meal.name,
+                              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                          Text(meal.description, style: new TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal)),
+                          Text(""),Text(""),Text(""),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0.2, 10.0, 0.0, 0.0),
+                            child: new Text("115 HRK",
+                                style: new TextStyle(
+                                    fontSize: 13.0, fontWeight: FontWeight.bold, color: Colors.black)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+          /*child: new Column(
+            children: <Widget>[
+               ListTile(
+                leading: Image.asset("assets/images/hamburger.jpg"),
+                title: Text(meal.name,
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),),
+                subtitle: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(meal.description, style: new TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.star, color: Colors.yellow[500],size: 15.0),
+                    Icon(Icons.star, color: Colors.yellow[500],size: 15.0),
+                    Icon(Icons.star, color: Colors.yellow[500],size: 15.0),
+                    Icon(Icons.star, color: Colors.grey,size: 15.0),
+                    Icon(Icons.star, color: Colors.grey,size: 15.0),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(48.0, 10.0, 0.0, 0.0),
+                      child: new Text("Cijena",
+                          style: new TextStyle(
+                              fontSize: 13.0, fontWeight: FontWeight.bold, color: Colors.black)),
+                    ),
+                  ],
+                ),
+              ),
+            ]
+          )*/
+        ),
       );
     },
     separatorBuilder: (context, index) => Divider(
-      color: Colors.black38,
+      color: Colors.grey,
     ),
   );
 }
