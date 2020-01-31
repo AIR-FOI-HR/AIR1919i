c0056e03c8ca6ea4cb1799be6e69c4defd1fd6f3
Toni Krešić <tonikresic1997@gmail.com>
2020-01-31 12:25:46 +0100

    Backend/database/migrations: [rename] - migration for qr_codes table

--- Backend/database/migrations/2020_01_31_112134_create_table_qr_code_user.php
+++ Backend/database/migrations/2020_01_31_112134_create_table_qr_code_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class QrCodeUser extends Migration
+class CreateTableQrCodeUser extends Migration
 {
     /**
      * Run the migrations.
@@ -17,8 +17,8 @@ class QrCodeUser extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('qr_code_id')->unsigned();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
             $table->foreign('qr_code_id')->references('id')->on('qr_codes')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
 
             $table->primary(['user_id', 'qr_code_id']);
         });
