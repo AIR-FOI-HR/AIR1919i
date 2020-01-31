43eea630c898a73fbeddf86b771f7784cc4932a5
Toni Krešić <tonikresic1997@gmail.com>
2020-01-31 16:44:21 +0100

    Backend/app/Rating: [add] - fillable attributes for mass assignment

--- Backend/app/Rating.php
+++ Backend/app/Rating.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'meal_id', 'user_id', 'stars', 'comment'
+    ];
+
+    /**
      * Rating HAS ONE User
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
