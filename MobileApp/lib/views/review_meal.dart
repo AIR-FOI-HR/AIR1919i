94d965a4f555a368a8f9c0006fb28cffa910d7fb
clakupres <clakupres@foi.hr>
2020-02-02 19:32:45 +0100

    Izmjena u review_meal - dodana inicijalna lista reviews i raised button za pregled više komentara

--- MobileApp/lib/views/review_meal.dart
+++ MobileApp/lib/views/review_meal.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/models/meal.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ReviewMeal extends StatelessWidget{
   @override
 
   Widget build(BuildContext context) {
+
+    Future<String> _getUserImage() async{
+      final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
+      return sharedPrefs.getString('img');
+    }
+
     return Scaffold(
       appBar: AppBar(
           title: Text('Meal Detail'),
@@ -20,67 +26,148 @@ class ReviewMeal extends StatelessWidget{
             )
           ]),
       body: Container(
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            Image(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/images/hamburger.jpg"),
-              height: MediaQuery.of(context).size.width * 0.5,
-              width: MediaQuery.of(context).size.width,
+            Stack(
+              children: <Widget>[
+                Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/hamburger.jpg"),
+                  height: MediaQuery.of(context).size.width * 0.35,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.width * 0.35,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color(0x00000000),
+                        const Color(0x00000000),
+                        const Color(0xCC000000),
+                        const Color(0xCC000000),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 44.0,
+                    left: 8.0,
+                    child: Text("Hamburger",
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18))),
+                Positioned(
+                    bottom: 28.0,
+                    left: 8.0,
+                    child: Text("Sadrazaj",style: TextStyle(color: Colors.white))),
+                Positioned(
+                  bottom: 8.0,
+                  left: 8.0,
+                  child: Row(
+                    children: [
+                      Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
+                      Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
+                      Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
+                      Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
+                      Icon(Icons.star, color: Colors.grey,size: 16.0),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 8.0,
+                  right: 8.0,
+                  child: Row(
+                    children: <Widget>[
+                      Text("87 ", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 13)),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 16.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: MediaQuery.of(context).size.width * 0.5,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    const Color(0x00000000),
-                    const Color(0x00000000),
-                    const Color(0x00000000),
-                    const Color(0xCC000000),
-                    const Color(0xCC000000),
-                  ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 25, 10, 10),
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(80.0),
+                  child: FutureBuilder<String>(
+                      future: _getUserImage(),
+                      initialData: 'http://192.168.0.43:8000/img/DefaultUserImage.png',
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        return snapshot.hasData ?
+                        Image.network(snapshot.data != 'NO_IMAGE_SET' ? snapshot.data : 'http://192.168.0.43:8000/img/DefaultUserImage.png', width: 100) :
+                        CircularProgressIndicator();
+                      }
+                  ),
                 ),
               ),
             ),
-            Positioned(
-                bottom: 48.0,
-                left: 8.0,
-                child: Text("Hamburger",
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18))),
-            Positioned(
-                bottom: 29.0,
-                left: 8.0,
-                child: Text("Sadrazaj",style: TextStyle(color: Colors.white))),
-            Positioned(
-              bottom: 10.0,
-              left: 8.0,
+            Center(child: Text("Rate and review", style: TextStyle(fontWeight: FontWeight.bold))),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,8,0,2),
               child: Row(
-                  children: [
-                    Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
-                    Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
-                    Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
-                    Icon(Icons.star, color: Color(0xffFFB200),size: 16.0),
-                    Icon(Icons.star, color: Colors.grey,size: 16.0),
-                  ],
-                ),
-            ),
-            Positioned(
-              bottom: 10.0,
-              right: 8.0,
-              child: Row(
-                children: <Widget>[
-                  Text("87 ", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 13)),
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                    size: 16.0,
-                  ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star_border, color: Colors.yellow[500],size: 25.0),
+                  Icon(Icons.star_border, color: Colors.yellow[500],size: 25.0),
+                  Icon(Icons.star_border, color: Colors.yellow[500],size: 25.0),
+                  Icon(Icons.star_border, color: Colors.yellow[500],size: 25.0),
+                  Icon(Icons.star_border, color: Colors.yellow[500],size: 25.0),
                 ],
               ),
             ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Recent Reviews",style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),)
+                        ],
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("255 total",style: TextStyle(color: Colors.grey, fontSize: 14),)
+                        ],
+                      )),
+                ],
+              ),
+            ),
+            Container(
+              height: 220,
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index){
+                  return ListTile(
+                    leading: Icon(Icons.person, size: 30),
+                    title: Text("Ime Prezime"),
+                    subtitle: Text("Komentar....")
+                  );
+                }
+              )
+            ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: RaisedButton(
+                color: Colors.orange.shade500,
+                onPressed: () {},
+                child: Text('Load More Reviews', style: TextStyle(fontSize: 14, color: Colors.white))),
+          ),
+        ),
           ],
         ),
       )
