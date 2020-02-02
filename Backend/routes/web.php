ddab31e4f4b135b033d0369690c77d000dea9b0d
clakupres <clakupres@foi.hr>
2020-02-02 12:36:08 +0100

    dodan api endpoint za slanje notifikacija

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function () { return view('/welcome');});
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/testing', 'TestingController@index');
+
+Route::post('/send-notification', 'NotificationController@index')->name('send-notification');
